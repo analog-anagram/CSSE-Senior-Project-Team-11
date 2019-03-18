75d04ce935c6f2b89481724a32f09ac95dd6875a
unknown <charlieadi@gmail.com>
2019-03-18 14:07:10 -0500

    changes to the dial, still need to do the math to do projection

--- UI/UI-All-In-One/Assets/Scripts/UI_Knob.cs
+++ UI/UI-All-In-One/Assets/Scripts/UI_Knob.cs
@@ -4,7 +4,7 @@ using UnityEngine.EventSystems;
 namespace UnityEngine.UI.Extensions
 {
     [RequireComponent(typeof(Image))]
-    public class UI_Knob : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler, IBeginDragHandler, IDragHandler
+    public class UI_Knob : MonoBehaviour
     {
         public enum Direction { CW, CCW };
         public Direction direction = Direction.CW;
@@ -21,6 +21,26 @@ namespace UnityEngine.UI.Extensions
         private Quaternion _initRotation;
         private bool _canDrag = false;
 
+        public bool onEnterUseForHover = false;
+        public bool onEnterUseForActivation = false;
+        public UnityEvent onEnter;
+
+        public bool onStayUseForHover = false;
+        public bool onStayUseForActivation = false;
+        public UnityEvent onHover;
+
+        public bool onExitUseForHover = false;
+        public bool onExitUseForActivation = false;
+        public UnityEvent onExit;
+
+        public bool detectOnEnter = true;
+        public bool detectOnHover = true;
+        public bool detectOnExit = true;
+        public LayerMask hoverLayer;
+        public LayerMask activationLayer;
+
+        public bool disabled;
+        /*
         //ONLY ALLOW ROTATION WITH POINTER OVER THE CONTROL
         public void OnPointerDown(PointerEventData eventData)
         {
@@ -38,17 +58,131 @@ namespace UnityEngine.UI.Extensions
         {
             _canDrag = false;
         }
-        public void OnBeginDrag(PointerEventData eventData)
+        */
+        private void OnCollisionEnter(Collision coll)
+        {
+            coll.contacts;
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (detectOnEnter && !disabled)
+            {
+
+                //LayerMask value is 2 to the power of its layer index
+                if (Mathf.Pow(2, other.gameObject.layer) == activationLayer.value && onEnterUseForActivation)
+                {
+                    Debug.Log("Act Layer Enter");
+                    if (onEnter != null)
+                    {
+                        SetInitPointerData(other);
+                        onEnter.Invoke();
+                    }
+                }
+
+                else if (Mathf.Pow(2, other.gameObject.layer) == hoverLayer.value && onEnterUseForHover)
+                {
+                    Debug.Log("Hover Layer Enter");
+                    if (onEnter != null)
+                    {
+                        onEnter.Invoke();
+                    }
+                }
+
+                else if (onEnterUseForActivation && onEnterUseForHover)
+                {
+                    Debug.Log("Both Layer Enter");
+                    if (onEnter != null)
+                    {
+                        onEnter.Invoke();
+                    }
+                }
+            }
+        }
+        void OnTriggerStay(Collider other)
+        {
+            if (detectOnHover && !disabled)
+            {
+                if (Mathf.Pow(2, other.gameObject.layer) == activationLayer.value && onStayUseForActivation)
+                {
+                    Debug.Log("Act Layer Stay");
+                    if (onHover != null)
+                    {
+                        onHover.Invoke();
+                    }
+                }
+
+                else if (Mathf.Pow(2, other.gameObject.layer) == hoverLayer.value && onStayUseForHover)
+                {
+                    Debug.Log("Hover Layer Stay");
+                    if (onHover != null)
+                    {
+                        onHover.Invoke();
+                    }
+                }
+                else if (onStayUseForActivation && onStayUseForHover)
+                {
+                    Debug.Log("Both Layer Enter");
+                    if (onHover != null)
+                    {
+                        onHover.Invoke();
+                    }
+                }
+            }
+
+        }
+        void OnTriggerExit(Collider other)
+        {
+            if (detectOnExit && !disabled)
+            {
+                if (Mathf.Pow(2, other.gameObject.layer) == activationLayer.value && onExitUseForActivation)
+                {
+                    Debug.Log("Act Layer Exit");
+                    if (onExit != null)
+                    {
+                        onExit.Invoke();
+                    }
+                }
+
+                else if (Mathf.Pow(2, other.gameObject.layer) == hoverLayer.value && onExitUseForHover)
+                {
+                    Debug.Log("Hover Layer Exit");
+                    if (onExit != null)
+                    {
+                        onExit.Invoke();
+                    }
+                }
+                else if (onExitUseForActivation && onExitUseForHover)
+                {
+                    Debug.Log("Both Layer Enter");
+                    if (onExit != null)
+                    {
+                        onExit.Invoke();
+                    }
+                }
+            }
+        }
+        public void OnBeginDrag(Collider eventData)
         {
             SetInitPointerData(eventData);
         }
-        void SetInitPointerData(PointerEventData eventData)
+        void SetInitPointerData(Collider eventData)
         {
             _initRotation = transform.rotation;
-            _currentVector = eventData.position - (Vector2)transform.position;
+            Vector3 position = new Vector3(0, 0, 0);
+            ContactPoint[] contacts = new ContactPoint[5];
+            eventData.GetContacts(contacts);
+            foreach(ContactPoint i in contacts)
+            {
+                position += i.point;
+            }
+            int len = eventData.contacts.length;
+            position /= len;
+            Vector3 zeroz = transform.position;
+            zeroz.z = 0;
+            _currentVector = position - zeroz;
             _initAngle = Mathf.Atan2(_currentVector.y, _currentVector.x) * Mathf.Rad2Deg;
         }
-        public void OnDrag(PointerEventData eventData)
+        public void OnDrag(Collider eventData)
         {
             //CHECK IF CAN DRAG
             if (!_canDrag)
